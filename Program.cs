baa251b72c2ca7fec6def5f797c56e445dc29111
Пользователь <@-PC>
2023-05-18 16:50:21 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("введите катет ");
+double kat=double.Parse(Console.ReadLine());
+Console.WriteLine("Введите гипотенузу ");
+double git=double.Parse(Console.ReadLine());
+double kat_2 = Math.Sqrt(git * git - kat * kat);
+Console.WriteLine($"Второй катет = {kat_2:f2}");
